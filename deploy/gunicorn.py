0556a4034f098bb9647aff430ffea03a9291d1ef
Udbhav Gupta <gupta.udbhav@gmail.com>
2012-10-05 11:04:34 -0400

    added gunicorn deploy

---
+++ deploy/gunicorn.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+sys.path.append("/home/udbhav/www/django/eurorack-planner/app/source/")
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+# This application object is used by the development server
+# as well as any WSGI server configured to use this file.
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
