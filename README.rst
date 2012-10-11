616e87022728cc2bb54a45b08875ca2c4583ca73
Udbhav Gupta <gupta.udbhav@gmail.com>
2012-10-11 15:00:35 -0400

    README and LICENSE

---
+++ README.rst
@@ -0,0 +1,27 @@
+================
+Eurorack Planner
+================
+
+http://eurorackplanner.com
+
+Eurorack Planner is a Django project to help modular synthesis addicts plan the best way to spend all of their discretionary income.
+
+Setup
+-----
+
+* Use pip to install the requirements::
+
+  pip install requirements.txt
+
+* Create /source/settings/local.py (see /source/settings/local.example.py)
+
+* Sync your databases::
+
+  python manage.py syncdb
+  python manage.py migrate
+
+* Start your server::
+
+  python manage.py runserver
+
+
---
+++ source/settings/local.example.py
@@ -0,0 +1,32 @@
+from settings.default import *
+
+SITE_ID = 1
+DEBUG = True
+LOCAL_SERVE = True
+SOUTH_TESTS_MIGRATE = False
+
+# Dummy cache for development
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+# Set session engine to db so that our session doesn't get lost without cache
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
+
+# DB
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'eurorack-planner.sqlite'
+    }
+}
+
+# Debug Toolbar
+SHOW_DEBUG_TOOLBAR = True
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'HIDE_DJANGO_SQL': True,
+ }
+
