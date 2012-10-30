ac04791e3615e98c6215bd3a685019fc3548ce11
Udbhav Gupta <gupta.udbhav@gmail.com>
2012-10-30 16:40:12 -0400

    Remove sync_data scripts

--- scripts/sync_data.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash    
-source /home/udbhav/www/django/eurorack-planner/env/bin/activate
-export PYTHONPATH=/home/udbhav/www/django/eurorack-planner/app/source:$PYTHONPATH
-python /home/udbhav/www/django/eurorack-planner/app/scripts/sync_data.py
