28cde3a35a0356660ed50ec20ba9e1e5f015808b
Udbhav Gupta <gupta.udbhav@gmail.com>
2012-10-30 18:24:03 -0400

    admin refinements

--- source/apps/modules/admin.py
+++ source/apps/modules/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from apps.modules.models import *
 
 class ModuleAdmin(admin.ModelAdmin):
-    list_display = ['name', 'id', 'eurorackdb_id', 'manufacturer', 'updated', 'custom']
+    list_display = ['name', 'id', 'eurorackdb_id', 'manufacturer', 'updated', 'custom', 'current_12v', 'negative_current_12v', 'current_5v', 'msrp']
     list_filter = ['custom',]
     search_fields = ['name', 'manufacturer__name']
 
