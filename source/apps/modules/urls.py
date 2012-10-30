74f42fe1eb4c9ff7b6ee9dc5ba1ae9cc2f7894ff
Udbhav Gupta <gupta.udbhav@gmail.com>
2012-10-30 18:26:04 -0400

    disable modules url

--- source/apps/modules/urls.py
+++ source/apps/modules/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns(
     #url(r'^manufacturers/$', ManufacturersView.as_view(), {}, name='manufacturers'),
     #url(r'^manufacturers/(?P<pk>\d+)/modules/$', ModulesByManufacturer.as_view(), name='modules_by_manufacturer'),
 
-    url(r'^$', ModulesView.as_view(), {}, name='modules'),
+    #url(r'^$', ModulesView.as_view(), {}, name='modules'),
     url(r'^(?P<pk>\d+)/$', ModuleView.as_view(), name='module'),
     url(r'^json/(?P<pk>\d+)/$', JSONModuleView.as_view(), name='json_module'),
 
